fe2104ca72a80bc77839878154f097e6af66977a
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-02-17 09:28:01 +0100

    werki

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-// Importeer express uit de node_modules map
-import express from 'express';
+// Import the required modules
+import express from "express";
+import fetch from "node-fetch";
 
-// Maak een nieuwe express app aan
-const app = express()
+// Create a new Express app
+const app = express();
 
-const url = 'https://whois.fdnd.nl/api/v1/squad/squad-a-2022'
-const data = await fetch(url)
-  .then((response) => response.json())
-  .catch((error) => error)
+const url = 'https://whois.fdnd.nl/api/v1/squad/'
 
-console.log(data)
+// Set EJS as the template engine and specify the views directory
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-// Stel ejs in als template engine en geef de 'views' map door
-app.set('view engine', 'ejs')
-app.set('views', './views')
+// Serve static files from the public directory
+app.use(express.static("public"));
 
-// Gebruik de map 'public' voor statische resources
-app.use(express.static('public'))
+// Create a route for the index page
+app.get('/', function (request, response) {
+  console.log(request.query.squad)
 
-// Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index', data)
-})
+  let slug = request.query.squad || 'squad-a-2022'
+  let orderBy = request.query.orderBy || 'name' + '&direction=ASC'
+  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
 
-// Stel het poortnummer in waar express op gaat luisteren
-app.set('port', process.env.PORT || 8000)
+  fetchJson(squadUrl).then((data) => {
+    response.render('index', data)
+  })
+});
 
-// Start express op, haal het ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+// Set the port number and start the server
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Application available on: http://localhost:${port}`);
+});
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
