145d631e49ef5f6e613162bd4685aa86ee859986
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-02-22 10:56:44 +0100

    push

--- public/script.js
+++ public/script.js
@@ -18,24 +18,7 @@ priceInput.forEach(input =>{
         }
     });
 });
-rangeInput.forEach(input =>{
-    input.addEventListener("input", e =>{
-        let minVal = parseInt(rangeInput[0].value),
-        maxVal = parseInt(rangeInput[1].value);
-        if((maxVal - minVal) < priceGap){
-            if(e.target.className === "range-min"){
-                rangeInput[0].value = maxVal - priceGap
-            }else{
-                rangeInput[1].value = minVal + priceGap;
-            }
-        }else{
-            priceInput[0].value = minVal;
-            priceInput[1].value = maxVal;
-            range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
-            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-        }
-    });
-});
+
 
 
 
